4836cb44927d3a7d7d550ed62adf1f6c3d6de1f7
Tanas2K4 <bongbo355@gmail.com>
2025-06-05 01:31:14 +0700

    big update _layout.cshtml and add some file [GroupController, _layout.css

---
+++ ZEIN_TeamPlanner/Controllers/GroupController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TeamPlanner.Data;
+
+namespace ZEIN_TeamPlanner.Controllers
+{
+    public class GroupController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public GroupController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var groups = _context.Groups
+                .Include(g => g.Members)
+                .Include(g => g.Tasks)
+                .Include(g => g.Events)
+                .ToList();
+            return View(groups);
+        }
+    }
+}
